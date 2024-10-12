0c12872f7e5e2778a2648a50a1f26e5af393c1d9
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-10-12 15:35:06 +0700

    fix: vercel json

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,13 @@ import { HandlingError } from "./utils/HandlingError";
 const app = express();
 const port = environment.PORT || 8000;
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+  credentials: true,
+  preflightContinue: false
+}));
 app.use((req, res, next) => {
   if (["POST", "PUT", "PATCH", "DELETE"].includes(req.method)) {
     express.json()(req, res, next);
@@ -22,7 +28,7 @@ app.use((req, res, next) => {
 app.use(cookieParser());
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // app.use(bodyParser.json())
-app.use(express.json());
+// app.use(express.json());
 app.use(routes);
 app.use(HandlingError);
 
