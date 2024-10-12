7d912bdc3b486026ed46e0a243b45dfa28d83e81
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-10-12 17:01:43 +0700

    fix: vercel json

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,66 +5,18 @@ import { MESSAGE_CODE } from "../utils/MessageCode";
 
 const route = Router();
 
-/**
- * @swagger
- * /customers:
- *   get:
- *     summary: Get customer routes
- *     description: Endpoint for customer-related operations
- */
-route.use("/customers", costumerRoute);
+route.use("/customers", costumerRoute)
 
-/**
- * @swagger
- * /:
- *   get:
- *     summary: Welcome message
- *     responses:
- *       200:
- *         description: Returns a welcome message
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: Hello World 🚀
- */
 route.get("/", (req: Request, res: Response) => {
-  return res.json({ message: "Hello World 🚀" });
-});
+    return res.json({ message: "Hello World 🚀" })
+})
 
-/**
- * @swagger
- * /{any}:
- *   get:
- *     summary: 404 Error
- *     description: Handles any routes that are not defined
- *     responses:
- *       404:
- *         description: Route not found
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 status:
- *                   type: number
- *                   example: 404
- *                 code:
- *                   type: string
- *                   example: NOT_FOUND
- *                 message:
- *                   type: string
- *                   example: Route not found
- */
 route.use("*", (req: Request, res: Response) => {
-  return res.status(404).json({
-    status: 404,
-    code: MESSAGE_CODE.NOT_FOUND,
-    message: MESSAGES.ERROR.NOT_FOUND.ROUTE,
-  });
-});
+    return res.status(404).json({
+        status: 404,
+        code: MESSAGE_CODE.NOT_FOUND,
+        message: MESSAGES.ERROR.NOT_FOUND.ROUTE
+    })
+})
 
-export default route;
+export default route
